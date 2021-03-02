16ca0613fc93881e18205fc2f864c7f1afdbd58f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-03-02 18:43:36 +0800

    微信 Yaml 配置

---
+++ src/main/java/cn/org/weixin4j/core/Weixin4jYaml.java
@@ -0,0 +1,39 @@
+package cn.org.weixin4j.core;
+
+import lombok.Data;
+
+/**
+ * 微信 Yaml 配置
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Data
+public class Weixin4jYaml implements Weixin4j {
+
+    /**
+     * 开发者ID
+     */
+    private String appId;
+
+    /**
+     * 开发者密码
+     */
+    private String appSecret;
+
+    /**
+     * 服务器地址
+     */
+    private String url;
+
+    /**
+     * 令牌
+     */
+    private String token;
+
+    /**
+     * 消息加解密密钥
+     */
+    private String encodingAesKey;
+
+}
